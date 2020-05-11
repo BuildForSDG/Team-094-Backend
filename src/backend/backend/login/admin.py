6787215c37580dfff01f2fdc51a9a2e10b0a10b4
Extomvi <exceltadedokun@gmail.com>
2020-05-11 21:26:10 +0100

    "Added models"

--- src/backend/backend/login/admin.py
+++ src/backend/backend/login/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from django.contrib import admin 
+from djangoblog.login.models import Login
+
 # Register your models here.
+class LoginAdmin(admin.ModelAdmin): 
+    pass admin.site.register(Login, LoginAdmin)
+
