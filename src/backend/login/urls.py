25d49af6f3c95e5d99a3fa1ef42ff5766ea15f84
Extomvi <exceltadedokun@gmail.com>
2020-05-13 03:35:20 +0100

    "Updated urls.py"

--- src/backend/login/urls.py
+++ src/backend/login/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('api/login/', views.LoginListCreate.as_view() ),
+    path('api/login/', views.SocialSerializer.as_view() ),
 ]
