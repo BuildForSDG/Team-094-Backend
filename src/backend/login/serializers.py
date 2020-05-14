383f665325c0e2bd94b9339d39102fbe75152b5b
Extomvi <exceltadedokun@gmail.com>
2020-05-14 19:01:34 +0100

    "Updates"

--- src/backend/login/serializers.py
+++ src/backend/login/serializers.py
@@ -4,4 +4,4 @@ from .models import Login
 class LoginSerializer(serializers.ModelSerializer):
     class Meta:
         model = Login
-        fields = ('id', 'name', 'email', 'phone', 'location')
+        fields = ('id', 'name', 'email', 'phone', 'location', 'date')
