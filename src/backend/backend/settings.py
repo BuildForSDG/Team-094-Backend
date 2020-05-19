d3136d7b8dc6444bd2af8d108430cc39c03253d1
Ome Chukwuemeka <omeco4christ@gmail.comcd>
2020-05-19 18:17:56 +0000

    2nd commit

--- src/backend/backend/settings.py
+++ src/backend/backend/settings.py
@@ -132,7 +132,7 @@ AUTHENTICATION_BACKENDS = [
 # added this
 
 LOGIN_URL = 'login'
-LOGIN_REDIRECT_URL = 'home'
+LOGIN_REDIRECT_URL = 'profile'
 LOGOUT_URL = 'logout'
 LOGOUT_REDIRECT_URL = 'login'
 
@@ -149,8 +149,8 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'social_django.context_processors.backends', # add this
-                'social_django.context_processors.login_redirect', # add this
+                'social_django.context_processors.backends',  # add this
+                'social_django.context_processors.login_redirect',  # add this
             ],
         },
     },
@@ -158,9 +158,9 @@ TEMPLATES = [
 
 SOCIAL_AUTH_FACEBOOK_KEY = 683285375571053          # App ID
 SOCIAL_AUTH_FACEBOOK_SECRET = '4b1da0d9bc882f9d711ec4321f31fd60'       # App Secret
-SOCIAL_AUTH_FACEBOOK_SCOPE = ['email', 'user_link'] # add this
+SOCIAL_AUTH_FACEBOOK_SCOPE = ['email', 'user_link']  # add this
 SOCIAL_AUTH_FACEBOOK_PROFILE_EXTRA_PARAMS = {       # add this
-  'fields': 'id, name, email, picture.type(large), link'
+    'fields': 'id, name, email, picture.type(large), link'
 }
 SOCIAL_AUTH_FACEBOOK_EXTRA_DATA = [                 # add this
     ('name', 'name'),
