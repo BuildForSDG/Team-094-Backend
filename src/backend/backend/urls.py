bfb65258b9f09bd1a886d214a9e42f4341cebfae
Extomvi <exceltadedokun@gmail.com>
2020-06-07 19:46:42 +0100

    Changes to urls.py

--- src/backend/backend/urls.py
+++ src/backend/backend/urls.py
@@ -19,7 +19,8 @@ from django.contrib.auth import views as auth_views
 from account import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),         path("login/", views.login, name="login"),
+    path('admin/', admin.site.urls),         
+    path("login/", views.login, name="login"),
     path("logout/", auth_views.LogoutView.as_view(), name="logout"),
     path('social-auth/', include('social_django.urls', namespace="social")),
     path("", views.home, name="home"),
